5166fc4778a555fc328d9d428b498423ff5bc239
cgolobic <cgolobic@gmail.com>
2020-06-20 23:24:07 -0700

    add basic consumer that calls api

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,5 +1,27 @@
+import pika
+import time
+import random
+import requests
+
+def callback(ch, method, properties, body):
+    r = random.random()
+    denominator = (0, r * 10)[r < 0.5]
+    response = requests.post('http://api:5000/calculate', json={
+        'numerator': 5,
+        'denominator': denominator
+    })
+    time.sleep(0.1)  #<< causes unacked messages if too high (slow consumer).  Use QoS to buffer messages...
+    if response.status_code == 200:
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    else:
+        ch.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
+        
+
 def main():
-    print("hello from consumer")
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+    channel = connection.channel()
+    channel.basic_consume(queue='messages', on_message_callback=callback)
+    channel.start_consuming()
 
 if __name__ == "__main__":
     main()
--- producer/producer.py
+++ producer/producer.py
@@ -1,10 +1,14 @@
 import pika
+import json
+import time
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
     channel = connection.channel()
-    channel.basic_publish(exchange='messages.fanout', routing_key='', body='hello world')
-    print('sent message to fanout queue')
+    while True:
+        payload = json.dumps({ 'numerator': 10, 'denominator': 3 })
+        channel.basic_publish(exchange='messages.fanout', routing_key='', body=payload)
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
